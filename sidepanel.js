10ff6f6d06025f578cf4a18f832e8085909c0bc7
Egor-Pavlov <72335308+Egor-Pavlov@users.noreply.github.com>
2022-05-23 10:39:58 +0700

    Update sidepanel.js

--- sidepanel.js
+++ sidepanel.js
@@ -5,3 +5,5 @@ document.querySelector(".openbtn").addEventListener('click', () => {
 document.querySelector(".closebtn").addEventListener('click', () => {
     document.getElementById("sidepanel").style.width = "0";
 })
+
+document.querySelector('#Visualization').addEventListener('click', () => { document.querySelector('#sidepanel').style.width = 0;});
